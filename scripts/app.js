8719c4cdbaa1b5cb561d5b0bef7af60c42c75258
vlodkopits <vlodkopits@gmail.com>
2016-09-27 15:54:20 +0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,68 @@
+(function () {
+'use strict';
+
+angular.module('kidsAbc', [])
+.controller('WordsController', WordsController)
+.service('WordsService', WordsService);
+
+
+WordsController.$inject = ['WordsService'];
+function WordsController(WordsService) {
+  var word = this;
+
+  word.items = WordsService.getItems();
+
+  word.say = function (itemIndex) {
+    WordsService.say(itemIndex);
+  };
+}
+
+
+function WordsService() {
+  var service = this;
+
+  // List of words 
+  var items = [
+  	{name: "cat"},
+    {name:"dog"},
+    {name: "elephant"},
+    {name: "horse"},
+    {name: "penguin"},
+    {name: "zebra"},
+    {name: "duck"},
+    {name: "camel"},
+    {name: "cow"},
+    {name: "snake"},
+    {name: "giraffe"},
+    {name: "raccoon"},
+    {name: "leopard"},
+    {name: "rhinoceros"},
+    {name: "lion"},
+    {name: "fox"},
+    {name: "panda"},
+    {name: "kangaroo"},
+    {name: "dolphin"},
+    {name: "iguana"},
+    {name: "koala"},
+    {name: "tiger"},
+    {name: "owl"},
+    {name: "bear"},
+    {name: "ant"},
+    {name: "emu"},
+    {name: "spider"}
+  ];
+
+
+  service.say = function (itemIndex) {
+
+  	console.log (items[itemIndex].name);
+    responsiveVoice.speak(items[itemIndex].name);
+  };
+
+  service.getItems = function (){
+  	return items;
+  };
+ 
+}
+
+})();
